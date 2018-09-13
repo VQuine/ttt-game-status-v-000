a8997eb0379e27f802d26678654bd3d6443524fe
Kimberly Aviles <01010110.v@gmail.com>
2018-09-13 21:56:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
     if board[combo[0]] == board[combo[1]] && board[combo[2]] == board[combo[1]] && position_taken?(board, combo[0])
-      return true
+      return combo
     end
   end
   false
